fd8cc7df0880a445de20f667fb61771802240080
Robin Davies <rerdavies@gmail.com>
2022-04-23 12:12:30 -0400

    G+11 Warning on github

--- lib/OsLinux.cpp
+++ lib/OsLinux.cpp
@@ -284,7 +284,7 @@ void cotask::os::SetFileNonBlocking(int file_fd, bool nonBlocking)
 
 void cotask::os::SetThreadBackgroundPriority()
 {
-    auto ignored = nice(1);
+    (void)nice(1);
 }
 
 namespace cotask::os
